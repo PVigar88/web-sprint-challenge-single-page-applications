b1e4337f4de511d992ff014dbae6c7b3e84de94f
PVigar88 <Pvigar@gmail.com>
2021-04-17 14:08:44 -0400

    roughed out confirmation page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ const App = () => {
   return (
     <>
       <h1>Lambda Eats</h1>
+
+      <div>
+        Your Favorite Food, Delivered While Coding
+        {/* button or link here */}
+      </div>
+
+      <h3>Food Delivery Near here</h3>
+
       <PizzaForm></PizzaForm>
     </>
   );
---
+++ src/components/confirmation.js
@@ -0,0 +1,12 @@
+export default function Confirmation(props) {
+  return (
+    <div className="confirmationPage">
+      <h2>Congrats! Pizza is on its way!</h2>
+
+      <div>
+        <h3>While you wait, how about a dog with a pizza</h3>
+        <img />
+      </div>
+    </div>
+  );
+}
