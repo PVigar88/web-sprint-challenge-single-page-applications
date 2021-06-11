ecb92b9f1978815181b7497ef18876c4d50e0ab9
PVigar88 <Pvigar@gmail.com>
2021-06-10 21:42:19 -0400

    another update

---
+++ build/precache-manifest.4b517b6c57426e191c46e7878f5ae19b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e972bb13a7da316e5452081a490454",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ece69230c36399a10a8a",
+    "url": "/static/css/main.b9a5676c.chunk.css"
+  },
+  {
+    "revision": "9205cb22481006328da9",
+    "url": "/static/js/2.5cdc448d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5cdc448d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ece69230c36399a10a8a",
+    "url": "/static/js/main.6e3025c4.chunk.js"
+  },
+  {
+    "revision": "545a27a708665afc91a8",
+    "url": "/static/js/runtime-main.e36952ae.js"
+  },
+  {
+    "revision": "c66f6ea9916751362586e93e150c4dd2",
+    "url": "/static/media/Pizza.c66f6ea9.jpg"
+  }
+]);
